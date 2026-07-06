6adeb74619344359b5885544a1695e3e8237f5b4
marcel <izuobasomtochukwu@gmail.com>
2026-07-06 18:37:26 +0100

    set up favicon config

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,12 +9,16 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "FudStack - The Chef's key to seamless workflow",
-  description: "An AI powered multi-tenant e-commerce app making  workflows in restaurants fast , reliable and seamless",
-  icons:{
-    icon:[{url:"./foodstack.png" , type:"image/png" , sizes:"512x512"}],
-     shortcut: [{ url: "/icon.png", type: "image/png", sizes: "512x512" }],
+  description: "An AI powered multi-tenant e-commerce app making workflows in restaurants fast , reliable and seamless",
+  icons: {
+    icon: [{ url: "/icon.png", type: "image/png", sizes: "512x512" }],
+    apple: [
+      { url: "/icon.png", sizes: "180x180", type: "image/png" }
+    ],
+    shortcut:[{url:"/icon.png" , sizes:"512x512" , type:"image/png"}]
   }
 };
+
 
 export default function RootLayout({
   children,
