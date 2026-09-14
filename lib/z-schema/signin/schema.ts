d878cf86edfc38b5baedf283fb3aa79a8e87f209
marcel <izuobasomtochukwu@gmail.com>
2026-09-14 16:53:26 +0100

    added server side validation with zod

---
+++ lib/z-schema/signin/schema.ts
@@ -0,0 +1,8 @@
+"use server";
+import {z} from "zod";
+export const signInSchema = z.object({
+  name: z.string().min(5 , 'Name field must at least be 5 characters long!'),
+  email: z.email('Enter a valid email address'),
+  password : z.string().min(8 , 'Password field must be more than 8 characters!'),       
+})
+export type signInInput = z.infer<typeof signInSchema> 
